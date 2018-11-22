90ec02b1da0b05d7b2f04b0ddd6dd2c3456d2265
ValerianWeiss <valerian97@web.de>
2018-11-22 16:26:42 +0100

    added missing brackets

--- db/queries/queryResponses.go
+++ db/queries/queryResponses.go
@@ -3,8 +3,8 @@ package queries
 // FindResponse represents the structure of the response
 // body, which is gettin send by the db function
 type FindResponse struct {
-	Status int `json:"status`
+	Status int `json:"status"`
 	Data   struct {
-		Result []interface `json:"result"`
+		Result []interface{} `json:"result"`
 	} `json:"data"`
 }
