81083e2599935b4a9cdd071f3e4e38e763dbea16
ValerianWeiss <valerian97@web.de>
2018-11-29 09:58:07 +0100

    added insert response

--- db/queries/queryResponses.go
+++ db/queries/queryResponses.go
@@ -1,10 +1,20 @@
 package queries
 
 // FindResponse represents the structure of the response
-// body, which is gettin send by the db function
+// body, which is getting send by the db function on /find
 type FindResponse struct {
 	Status int `json:"status"`
 	Data   struct {
 		Result []interface{} `json:"result"`
 	} `json:"data"`
+}
+
+// InsertResponse represents the structure of the response
+// body, which is getting send by the db function on /insert
+type InsertResponse struct {
+	Result struct {
+		Ok          int `json:"ok"`
+		RecordCount int `json:"n"`
+	} `json:"result"`
+	InsertedRecords []interface{} `json:"ops"`
 }
